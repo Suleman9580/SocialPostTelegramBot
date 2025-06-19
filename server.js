56d19557559b696838fdc6feec320c24a003057d
Mohd. Suleman <sulemnmohd345@gmail.com>
2025-06-19 10:29:49 +0530

    Fixed Post Format

--- server.js
+++ server.js
@@ -118,11 +118,11 @@ bot.command('generate', async(ctx) => {
                     role: 'user',
                     content: `write like a human, for humans, craft three engaging socialmedia posts for linkedIn, Twitter (X) and facebook audiences, use simple language. use given time labels just to understand the order of the events. don't mention the time in the posts. Each posts should creatively highlights the following events. Ensure the tone is conversational and impactfull. Focus on engaging the respective platform's audience.
                     don't share unnecessary things just share things which anyone can just copy and paste. Add some emojis for more user attention, encourage interaction, sharing and driving interest in the events: ${event.map((event) => event.text).join(', ')}. The format should look like this:
-                    LinkedIn Post:
+                    🔹 LinkedIn Post:
                     -Generated LinkedIn post here.
-                    Twitter Post:
+                    🔹 Twitter(X) Post:
                     -Generated Twitter post here.
-                    Facebook Post:
+                    🔹 Facebook Post:
                     -Generated Facebook post here.
                     `
                 }
